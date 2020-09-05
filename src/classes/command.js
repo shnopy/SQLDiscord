7f56bc8a39406d2404c4a4247a8a4f5e6a22955f
shnopy <shnopy319@gmail.com>
2020-09-05 11:10:57 +0100

    Added message collector

--- src/classes/command.js
+++ src/classes/command.js
@@ -3,5 +3,13 @@ module.exports = class {
     this.message = message;
     this.args = args.map(v => v.toLowerCase());
     this.prefix = prefix;
+    this.collector = () => {
+      let c = message.channel.createMessageCollector((m) => m.author.id === this.message.author.id, { time: 10000 });
+      c.on("end", (collected) => {
+        if (!collected.size) return this.message.reply("Cancelled: No reply received");
+      });
+
+      return c;
+    };
   }
 };
